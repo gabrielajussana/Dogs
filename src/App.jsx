185162a3181c0fa09eff5adb66bbc5bdbcf27a3e
Gabriela Jussana <gabrielajussana@gmail.com>
2023-05-16 13:07:32 -0300

    feat: config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import React from 'react';
+
+const App = () => {
+  return <div>App React</div>;
+};
+
+export default App;
