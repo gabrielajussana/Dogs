9afbe2768c87ce0d22fb916dba2e8afe9f661702
Gabriela Jussana <gabrielajussana@gmail.com>
2023-05-24 11:02:52 -0300

    feat: comments

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments)
+  const { login } = React.useContext(UserContext);
+  const commentsSection = React.useRef(null);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeigth;
+  }, [comments])
+
   return (
-    <div>
-      
-    </div>
+   <>
+    <ul ref={commentsSection} className={styles.comment}>
+      {comments.map(comment => <li key={comment.id}>
+        <b>{comment.comment_author}: </b>
+        <span>{comment.comment_content}</span>
+      </li>
+      )}
+    </ul>
+    {login && <PhotoCommentsForm id={props.id} setComments={setComments}/> }
+   </>
   )
+}
+
+PhotoComments.propTypes = {
+  props: PropTypes.node.isRequired,
+  id: PropTypes.node.isRequired,
+  comments: PropTypes.node.isRequired,
 }
 
 export default PhotoComments
---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import useFetch from '../../Hooks/useFetch';
+import { COMMENT_POST } from '../../api';
+import PropTypes from 'prop-types';
+import Error from '../../Helper/Error';
+import styles from './PhotoCommentsForm.module.css';
+import {ReactComponent as Enviar} from '../../Assets/enviar.svg';
+
+const PhotoCommentsForm = ({id, setComments}) => {  
+  const [comment, setComment] = React.useState('');
+  const {request, error} = useFetch();
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, {comment})
+    const { response, json }= await request(url, options);
+    if(response.ok) {
+        setComment('');
+        setComments((comments) => [...comments, json]) 
+    } 
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <textarea 
+        id="comment"
+        name="comment"
+        value={comment} 
+        placeholder="Comente..."
+        className={styles.textarea}
+        onChange={({target}) => setComment(target.value)}  
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  )
+}
+
+PhotoCommentsForm.propTypes = {
+    id: PropTypes.node.isRequired,
+    setComments: PropTypes.node.isRequired,
+}
+
+export default PhotoCommentsForm;
